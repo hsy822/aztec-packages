c6fbffe7a9022f07a87442f5b5daa7df9a6b6d17
Alex Gherghisan <alexg@aztecprotocol.com>
2025-01-08 16:36:52 +0000

    test: provern node is able to recover after a crash

--- yarn-project/end-to-end/src/spartan/utils.ts
+++ yarn-project/end-to-end/src/spartan/utils.ts
@@ -16,6 +16,8 @@ const logger = createLogger('e2e:k8s-utils');
 const k8sLocalConfigSchema = z.object({
   INSTANCE_NAME: z.string().min(1, 'INSTANCE_NAME env variable must be set'),
   NAMESPACE: z.string().min(1, 'NAMESPACE env variable must be set'),
+  HOST_PROVER_NODE_PORT: z.coerce.number().min(1, 'HOST_PROVER_NODE_PORT env variable must be set'),
+  CONTAINER_PROVER_NODE_PORT: z.coerce.number().default(8080),
   HOST_PXE_PORT: z.coerce.number().min(1, 'HOST_PXE_PORT env variable must be set'),
   CONTAINER_PXE_PORT: z.coerce.number().default(8080),
   HOST_ETHEREUM_PORT: z.coerce.number().min(1, 'HOST_ETHEREUM_PORT env variable must be set'),
@@ -297,6 +299,25 @@ export function applyProverFailure({
     values: {
       'proverFailure.duration': `${durationSeconds}s`,
     },
+    logger,
+  });
+}
+
+export function applyProverKill({
+  namespace,
+  spartanDir,
+  logger,
+}: {
+  namespace: string;
+  spartanDir: string;
+  logger: Logger;
+}) {
+  return installChaosMeshChart({
+    instanceName: 'prover-kill',
+    targetNamespace: namespace,
+    valuesFile: 'prover-kill.yaml',
+    helmChartDir: getChartDir(spartanDir, 'aztec-chaos-scenarios'),
+    clean: true,
     logger,
   });
 }
