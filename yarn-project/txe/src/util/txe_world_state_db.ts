1d18d0f3f67d5332f3841256f2ce105261d8d594
dbanks12 <david@aztecprotocol.com>
2025-02-12 19:26:17 +0000

    fix txe

--- yarn-project/txe/src/util/txe_world_state_db.ts
+++ yarn-project/txe/src/util/txe_world_state_db.ts
@@ -32,12 +32,10 @@ export class TXEWorldStateDB extends WorldStateDB {
     return value;
   }
 
-  override async storageWrite(contract: AztecAddress, slot: Fr, newValue: Fr): Promise<bigint> {
+  override async storageWrite(contract: AztecAddress, slot: Fr, newValue: Fr): Promise<void> {
     await this.txe.addPublicDataWrites([
       new PublicDataWrite(await computePublicDataTreeLeafSlot(contract, slot), newValue),
     ]);
-
-    return newValue.toBigInt();
   }
 
   override checkpoint(): Promise<void> {
